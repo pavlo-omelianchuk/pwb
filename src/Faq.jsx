7bdbb5d23d78afa828479e3fd0604f2c51f5963d
Pavlo Omelianchuk <pavlo-omelianchuk>
2022-11-08 21:15:16 +0000

    about us page

--- src/Faq.jsx
+++ src/Faq.jsx
@@ -1,9 +1,29 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 export const FaqComponent = () => {
   const [toggle, setToggle] = useState(false);
   const handleToggle = () => {
     setToggle((prev) => !prev);
   };
-  return <button onClick={handleToggle}>{!toggle ? "Not Toggled" : "Toggled"}</button>;
+  const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+  `;
+
+  // Create a Wrapper component that'll render a <section> tag with some styles
+  const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+  `;
+
+  return (
+    <>
+      <button onClick={handleToggle}>{!toggle ? "Not Toggled" : "Toggled"}</button>
+      <Wrapper>
+        <Title>Hello World!</Title>
+      </Wrapper>
+    </>
+  );
 };
