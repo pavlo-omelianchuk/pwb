aac57d4a4f3ce0fe97256d5f38042dbf88997d8a
Pavlo Omelianchuk <pavlo-omelianchuk>
2022-11-18 01:47:25 +0000

    feat(pricing calculator): display results

--- src/PricingCalculator/PricingCalculator.tsx
+++ src/PricingCalculator/PricingCalculator.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import Box from '@mui/material/Box';
 import { SliderThumb } from '@mui/material/Slider';
 import { useEffect, useState } from 'react';
 import { GMV_RATE, WEEKS_IN_MONTH } from 'src/helpers/constants';
